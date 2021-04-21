7d0472b50ad7013a95c88e1671ae02f5126ae843
Godinson1 <josephgodwin274@yahoo.com>
2021-04-21 01:16:51 +0100

    Add login and register screen

--- src/Pages/Authentication/Login.tsx
+++ src/Pages/Authentication/Login.tsx
@@ -1,12 +1,24 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Link } from "react-router-dom";
 import "./auth.scss";
 
 const Login: FC = () => {
+  const [error, setError] = useState<boolean | string>(false);
   return (
     <div>
       <div className="auth">
+        <div className="flex-between">
+          <div className="ring"></div>
+          <div className="ring-two"></div>
+        </div>
         <div className="login">
+          {error && (
+            <div className="ui error message">
+              <i onClick={() => setError(false)} className="close icon"></i>
+              <div className="header">Error trying to Login..</div>
+              <p>{error}</p>
+            </div>
+          )}
           <h1 className="login-logo">MoneyBizz</h1>
           <div className="login-container">
             <div className="login-header">
@@ -32,6 +44,10 @@ const Login: FC = () => {
               </p>
             </div>
           </div>
+        </div>
+        <div className="flex-between">
+          <div className="ring-three"></div>
+          <div className="ring-four"></div>
         </div>
       </div>
     </div>
--- src/Pages/Authentication/Register.tsx
+++ src/Pages/Authentication/Register.tsx
@@ -1,20 +1,32 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Link } from "react-router-dom";
 import "./auth.scss";
 
 const Register: FC = () => {
+  const [error, setError] = useState<boolean | string>(false);
   return (
     <div>
-      <div className="auth">
+      <div className="register-auth">
+        <div className="flex-between">
+          <div className="ring"></div>
+          <div className="ring-two"></div>
+        </div>
         <div className="register">
-          <h1 className="login-logo">MoneyBizz</h1>
+          {error && (
+            <div className="ui error message">
+              <i onClick={() => setError(false)} className="close icon"></i>
+              <div className="header">Error trying to Register..</div>
+              <p>{error}</p>
+            </div>
+          )}
+          <h1 className="register-logo">MoneyBizz</h1>
           <div className="login-container">
             <div className="login-header">
               <h2>Create a Secure Account</h2>
               <p>Begin the journey to endless financial possibilities..</p>
             </div>
             <div>
-              <label>FirstName</label>
+              <label>First Name</label>
               <div className="login-input">
                 <input type="text" />
               </div>
@@ -28,7 +40,7 @@ const Register: FC = () => {
             <div>
               <label>Email</label>
               <div className="login-input">
-                <input type="text" />
+                <input type="email" />
               </div>
             </div>
             <div>
@@ -40,7 +52,7 @@ const Register: FC = () => {
             <div>
               <label>Password</label>
               <div className="login-input">
-                <input type="text" />
+                <input type="password" />
               </div>
             </div>
             <div className="auth-button">Register</div>
@@ -50,6 +62,10 @@ const Register: FC = () => {
               </p>
             </div>
           </div>
+        </div>
+        <div className="flex-between">
+          <div className="ring-three"></div>
+          <div className="ring-four"></div>
         </div>
       </div>
     </div>
